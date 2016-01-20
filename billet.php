1cdd4859d7b8550f880bdea35496251db6a56984
Manon Noblet <manon.noblet@epitech.eu>
2016-01-20 08:33:27 +0100

    Create billet.php

---
+++ billet.php
@@ -0,0 +1,149 @@
+<?php require"function/verif_connexion.php";  ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="icon" type="image/png" href="images/favicon.png"/>
+		<link rel="stylesheet" href="css/style.css" />
+		<title>StartinG Blog</title>
+		<script src="JS/script.js"></script>
+	</head>
+
+	<body>
+		<header>
+				<div class="logo">
+					<a href="index.php"><img src="images/logo5.png" /></a>
+				</div>
+			
+			<div class="formulaire">
+			<?php if (!isset($_SESSION['pseudo'])) { ?>
+				<form method="POST" action="index.php">
+					<input  type="text" name="login" placeholder="User.."/>
+					<input  type="password" name="password" placeholder="password.."/>
+					<input type="submit" name="connexion" class="bouton" value="Connection" />
+				</form>
+					<?php }  	
+					else if (isset($_SESSION['pseudo']) &&  $_SESSION['status'] == "lecteur") { 
+					 
+						if (isset($_SESSION['pseudo'])) {
+		      				$req4 =$bdd->query('SELECT * FROM tp_user WHERE pseudo = "'.$_SESSION['pseudo'].'" ');
+		      				$req4->execute();
+
+		      				while ($dataValue = $req4->fetch()) {
+		      					$_SESSION['status'] = $dataValue['status'];
+		      				}
+		      			}
+					?>
+				<nav>
+					<ul>
+						<li><a href="index.php"> Accueil </a></li>
+					</ul>
+				</nav>
+			<?php }
+
+			else if (isset($_SESSION['pseudo']) && isset($_SESSION['status']) && $_SESSION['status'] == "auteur") {
+				
+						if (isset($_SESSION['pseudo'])) {
+		      				$req4 =$bdd->query('SELECT * FROM tp_user WHERE pseudo = "'.$_SESSION['pseudo'].'" ');
+		      				$req4->execute();
+
+		      				while ($dataValue = $req4->fetch()) {
+		      					$_SESSION['status'] = $dataValue['status'];
+		      					$_SESSION['id_user'] = $dataValue['id_user'];
+		      				}
+		      			}
+					?>
+				<nav>
+					<ul>
+						<li><a href="index.php"> Accueil </a></li>
+						<li><a href="billet.php"> Creation de Billet </a></li>
+						<li><a href="categorie.php"> Gestion des Categories </a></li>
+						<li><a href="gestion_poster.php"> Gestion des Postes </a></li>
+					</ul>
+				</nav>
+			<?php } 
+			else if (isset($_SESSION['pseudo']) && isset($_SESSION['status']) && $_SESSION['status'] == "admin") {
+				if (isset($_SESSION['pseudo'])) {
+		      				$req4 =$bdd->query('SELECT * FROM tp_user WHERE pseudo = "'.$_SESSION['pseudo'].'" ');
+		      				$req4->execute();
+
+		      				while ($dataValue = $req4->fetch()) {
+		      					$_SESSION['status'] = $dataValue['status'];
+		      					$_SESSION['id_user'] = $dataValue['id_user'];
+		      				}
+		      			}
+					?>
+				<nav>
+					<ul>
+						<li><a href="index.php"> Accueil </a></li>
+						<li><a href="billet.php"> Creation de Billet </a></li>
+						<li><a href="categorie.php"> Gestion des Categories </a></li>
+						<li><a href="gestion_user.php"> Postes et Users </a></li>
+						<li><a href="gestion_poster.php"> Gestion des Postes </a></li>
+					</ul>
+				</nav>
+			<?php }else if (isset($_SESSION['pseudo']) && isset($_SESSION['status']) && $_SESSION['status'] == "bannir") { 
+				echo"<script> alert('Vous avez été Banni Désolé !'); </script>";
+				?>
+				<form method="POST" action="index.php">
+					<input  type="text" name="login" placeholder="User.."/>
+					<input  type="password" name="password" placeholder="password.."/>
+					<input type="submit" name="connexion" class="bouton" value="Connection" />
+				</form>
+			<?php } ?>
+			</div>
+		</header>
+		<hr>
+		<article>
+				<div id="style_poste_user">
+					<?php if (!isset($_SESSION['pseudo'])) { ?>
+				<center><a href="inscription.php" style="color:#f00;font-weight: bold;">Creer un Compte</a></center>
+				<?php }elseif(isset($_SESSION['pseudo']) && $_SESSION['status'] != "bannir") { ?>
+				<p align="center"> Votre Ip : <?php echo $_SERVER["SERVER_ADDR"]; ?></p>
+					<p align="center">
+					<?php 
+						if (isset($_SESSION['pseudo']) && $_SESSION['status'] != "bannir") {
+		      				$req4 =$bdd->query('SELECT * FROM tp_user WHERE pseudo = "'.$_SESSION['pseudo'].'" ');
+		      				$req4->execute();
+		      				while ($dataValue = $req4->fetch()) {
+		      					$_SESSION['status'] = $dataValue['status'];
+		      					$_SESSION['photo'] = $dataValue['avatar'];
+		      				}
+		      			}
+					?>
+				<img src='uploader/<?php echo $_SESSION['photo']; ?>' class="avatar" />
+				<p/><p align="center"> Bienvenue : <?php echo $_SESSION['pseudo']."<br> Votre Status :".$_SESSION['status']; ?>  </p>
+				<p align="center"> <img src="images/logout.png" class="avs" />&nbsp;&nbsp;<font color="#ffffff"><a href="function/deconnexion.php">Se Deconnecter</a></font><br />
+				<?php }
+
+				if (isset($_SESSION['pseudo']) && $_SESSION['status'] == "bannir") { ?>
+		      				<center><a href="inscription.php" style="color:#f00;font-weight: bold;">Creer un Compte</a></center>
+		      	<?php } ?>
+				<hr />
+				</div>
+				<div id="style_poste">
+					<fieldset>
+					<?php require"function/poste_billet.php"; ?>
+						<legend>Poster un Billet</legend>
+						<form method="POST" action="billet.php" enctype="multipart/form-data">
+							<input type="text" name="titre" maxlength="150" class="text_billet" placeholder="Titre du Billet" required />
+							<input type="text" name="chapo" maxlength="200" class="text_billet" placeholder="Description" required/>
+							<input type="file" name="image" maxlength="150" class="text_billet" placeholder="lien de votre image" required />
+							<textarea name="corps" id="text_post" placeholder="Entrez votre Texte..." required></textarea>	
+							<select name="id_categorie" style="width:205px;height:40px;border:1px solid #ccc;">
+                                <option>Categories Une Categorie</option>
+                                  <?php 
+                                  $rep = $bdd->query("SELECT id_categorie,nom_categories FROM categorie");
+                                  while ($donnee = $rep->fetch()) {
+                                   echo "<option value=".$donnee['id_categorie'].">".$donnee['nom_categories']."</option>";
+                                  } ?>
+                                </select>
+							<input type="text" name="tags" class="text_billet" placeholder="#exemple #exemple #exemple" style="width:300px;" required/>
+							<input type="date" name="date_poster" class="text_billet" placeholder="yyyy-mm-jj" style="width:300px;" required/>
+							<input type="submit" value="Poster" name="send_billet" />
+						</form>
+					</fieldset>
+				</div>					
+		</article>
+	</body>
+	</html>
